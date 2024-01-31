b5d7d0d2a455e630bca6e4c5e5bc3cb50e65f264
Hisashi Peterson <146689382+sashipeterson@users.noreply.github.com>
2024-01-31 13:05:31 +0900

    Finished prompts

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // import the pets array from data.js
 const pets = require('./data');
 
+const path = require('path')
+
 // init express app
 const express = require('express');
 const app = express();
@@ -10,6 +12,7 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
+    res.sendFile(path.join(__dirname, 'public', '/index.html'));
 
 });
 
@@ -21,31 +24,32 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.json(pets);
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const owner = req.query.owner;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
+    res.send(pet);
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const name = req.params.name;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    res.send(pet)
 });
 
 app.listen(PORT, () => {
